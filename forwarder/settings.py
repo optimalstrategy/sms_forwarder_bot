2b588e10efc8209818ebecb9f15ce156c78c560a
justpianm8 <usangeorg@ya.ru>
2018-11-04 02:26:17 -0400

    Fixed typo in settings

--- forwarder/settings.py
+++ forwarder/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "True") != "False"
+DEBUG = os.environ.get("DJANGO_DEBUG", "True") != "False"
 
 BASE_HOST = os.environ.get("FWD_BASE_HOST")
 
