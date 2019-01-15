652169f004925c482ec657f0a3f964f5db19c680
optimalstrategy <17456182+OptimalStrategy@users.noreply.github.com>
2019-01-15 01:13:31 -0500

    updated requirements

--- forwarder/urls.py
+++ forwarder/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from bot.views import  *
+from bot.views import *
 from forwarder.settings import TELEGRAM_TOKEN
 
 urlpatterns = [
-    path(f"{TELEGRAM_TOKEN}/", web_hook_view, name="webhook"),
+    path(f"{TELEGRAM_TOKEN}", web_hook_view, name="webhook"),
     path("forward", forward_sms, name="forward"),
     path("check_user", check_user, name="check_user"),
     path("-/__heartbeat__", is_alive, name="__heartbeat__"),
