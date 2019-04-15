b7b159214b6c062c9882582a47d2fcac833b17ee
optimalstrategy <17456182+OptimalStrategy@users.noreply.github.com>
2019-04-15 10:48:32 -0400

    fix webhook endpoint

--- forwarder/urls.py
+++ forwarder/urls.py
@@ -4,7 +4,7 @@ from bot.views import *
 from forwarder.settings import TELEGRAM_TOKEN
 
 urlpatterns = [
-    path(f"{TELEGRAM_TOKEN}", web_hook_view, name="webhook"),
+    path(f"{TELEGRAM_TOKEN}/", web_hook_view, name="webhook"),
     path("forward", forward_sms, name="forward"),
     path("check_user", check_user, name="check_user"),
     path("-/__heartbeat__", is_alive, name="__heartbeat__"),
